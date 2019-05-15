ff50a63b19f9f370dbcdda1f000a3d0c8265e517
Jemil Gunay <Jemil.Gunay@bet365.com>
2019-05-15 16:25:45 +0100

    Removed black borders from wavey shader.

--- assets/shaders/wavey.frag.glsl
+++ assets/shaders/wavey.frag.glsl
@@ -11,9 +11,12 @@ uniform float uTime;
 
 void main() {
     vec2 t = (vTexCoords - uTexBounds.xy) / uTexBounds.zw;
-    vec3 influence = texture(uTexture, t).rgb;
-    t.y += cos(t.x * 40.0 + (uTime * uSpeed))*0.005;
-    t.x += cos(t.y * 40.0 + (uTime * uSpeed))*0.01;
+    float minBorder = 0.01;
+    float maxBorder = 1.0-minBorder;
+    if (t.x > minBorder && t.x < maxBorder && t.y > minBorder && t.y < maxBorder) {
+        t.y += cos(t.x * 40.0 + (uTime * uSpeed))*0.005;
+        t.x += cos(t.y * 40.0 - (uTime * uSpeed))*0.01;
+    }
     vec3 col = texture(uTexture, t).rgb;
-    fragColor = vec4(col,1.0);
+    fragColor = vec4(col, 1.0);
 }
